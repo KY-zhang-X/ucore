33bc2079077b0603ec7346cb87e78a9be271fbf4
KYzhang <tjkdbd@163.com>
2021-03-29 00:32:50 +0800

    comment dev_stdin.c

--- kern/fs/devs/dev_stdin.c
+++ kern/fs/devs/dev_stdin.c
@@ -14,10 +14,21 @@
 
 #define STDIN_BUFSIZE               4096
 
-static char stdin_buffer[STDIN_BUFSIZE];
-static off_t p_rpos, p_wpos;
-static wait_queue_t __wait_queue, *wait_queue = &__wait_queue;
+static char stdin_buffer[STDIN_BUFSIZE]; // stdin缓冲区
+static off_t p_rpos, p_wpos; // read position 和 write position 对应向缓冲区里读和写的位置
+static wait_queue_t __wait_queue, *wait_queue = &__wait_queue; // 等待队列, 存储阻塞在stdin上的进程
 
+
+/**
+ * 写stdin缓冲区stdin_buffer
+ * 在<trap.c>中的键盘中断处被调用
+ * 
+ * 输入缓冲区大小为 STDIN_BUFSIZE = 4096
+ * 如果stdin_buffer缓冲的字符数达到上限, 则最后写入的字符会持续被覆盖
+ * 写字符的时候会检测等待队列是否为空, 如果不为空就唤醒其中所有进程 (?: 唤醒其中所有进程)
+ * 上述操作均在中断的临界区内完成
+ * (p_rpos和p_wpos都是一直向前增长, 但是由于都采用了off_t即64位整数, 因此不需要担心溢出的问题)
+ **/
 void
 dev_stdin_write(char c) {
     bool intr_flag;
@@ -36,6 +47,15 @@ dev_stdin_write(char c) {
     }
 }
 
+/**
+ * 读stdin缓冲区stdin_buffer
+ * 在stdin_io处被调用
+ * 
+ * 从stdin缓冲区stdin_buffer中读长度为len的数据到缓冲区buf中
+ * 如果缓冲区内没有内容, 则调用wait_current_set将当前进程入队, 进程进入阻塞状态, 同时开启调度
+ * 当进程阻塞状态解除时, 检查wakeup_flags是否为WT_KBD, 符合则重新尝试从缓冲区中取数据, 否则退出
+ * 返回值是成功读取的字符个数
+ **/
 static int
 dev_stdin_read(char *buf, size_t len) {
     int ret = 0;
@@ -57,7 +77,7 @@ dev_stdin_read(char *buf, size_t len) {
                 local_intr_save(intr_flag);
                 wait_current_del(wait_queue, wait);
                 if (wait->wakeup_flags == WT_KBD) {
-                    goto try_again;
+                    goto try_again; // 使用goto的目的是, 防止执行for循环的ret++, p_rpos++语句
                 }
                 break;
             }
@@ -67,6 +87,12 @@ dev_stdin_read(char *buf, size_t len) {
     return ret;
 }
 
+/**
+ * 打开函数, 没有实际作用
+ * 对应struct device中的d_open函数
+ * 
+ * 检查stdin的只读标志
+ **/
 static int
 stdin_open(struct device *dev, uint32_t open_flags) {
     if (open_flags != O_RDONLY) {
@@ -75,11 +101,22 @@ stdin_open(struct device *dev, uint32_t open_flags) {
     return 0;
 }
 
+/**
+ * 关闭函数, 没有实际作用
+ * 对应struct device中的d_close函数
+ **/
 static int
 stdin_close(struct device *dev) {
     return 0;
 }
 
+/**
+ * 设备io接口函数, 只有读没有写
+ * 对应struct device中的d_io函数
+ * 
+ * 调用dev_stdin_read将stdin_buffer中的缓冲发送到iobuf中
+ * 返回实际读取到的字节数
+ **/
 static int
 stdin_io(struct device *dev, struct iobuf *iob, bool write) {
     if (!write) {
@@ -92,11 +129,19 @@ stdin_io(struct device *dev, struct iobuf *iob, bool write) {
     return -E_INVAL;
 }
 
+/**
+ * 设备io控制接口函数, 没有实际作用
+ * 对应struct device中的d_ioctl函数
+ **/
 static int
 stdin_ioctl(struct device *dev, int op, void *data) {
     return -E_INVAL;
 }
 
+/**
+ * 初始化stdin的设备结构体, 以及stdin缓冲区和等待队列
+ * 在dev_init_stdin处被调用
+ **/
 static void
 stdin_device_init(struct device *dev) {
     dev->d_blocks = 0;
@@ -110,6 +155,13 @@ stdin_device_init(struct device *dev) {
     wait_queue_init(wait_queue);
 }
 
+/**
+ * 初始化在vfs中的stdin设备
+ * 在dev_init中被调用
+ * 
+ * 为stdin创建一个inode, 将inode中的in_info字段作为struct device初始化
+ * 将inode加入到vfs的设备列表vdef_list中
+ **/
 void
 dev_init_stdin(void) {
     struct inode *node;
